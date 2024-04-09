2a2ff7828842958607ab48ece611481d567c5a7c
Nathanms1 <nathanms2000@outlook.com>
2024-04-09 01:25:09 -0300

    Aula Encapsulamento

--- aulasPOO/src/aulaPilaresPOO/MSNMessenger.java
+++ aulasPOO/src/aulaPilaresPOO/MSNMessenger.java
@@ -2,15 +2,19 @@ package aulaPilaresPOO;
 
 public class MSNMessenger {
 	public void enviarMensagem() {
+		validarConectadoInternet();
 		System.out.println("Enviando mensagem");
+		salvarHistoricoMensagem();
 	}
 	public void receberMensagem() {
+		validarConectadoInternet();
 		System.out.println("Recebendo mensagem");
+		salvarHistoricoMensagem();
 	}
-	public void validarConectadoInternet() {
+	private void validarConectadoInternet() {
 		System.out.println("Validando se está conectado a internet");
 	}
-	public void salvarHistoricoMensagem() {
+	private void salvarHistoricoMensagem() {
 		System.out.println("Salvando o histórico da mensagem");
 	}
 }
